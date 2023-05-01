49e0451a6686665f5fbf90d884796baec7ee8264
Crescenzo D'Alterio <crescenzo.dalterio@simplybusiness.co.uk>
2023-05-01 22:32:11 +0100

    add validation to event log

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -3,4 +3,7 @@
 class EventLog < ApplicationRecord
   belongs_to :property_enquiry
   broadcasts_to :property_enquiry
+
+  validates :from_state, presence: true
+  validates :to_state, presence: true
 end
